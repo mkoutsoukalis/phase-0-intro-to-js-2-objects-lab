5fff1f32717e7fd27af321846d1bd32e06bef252
Maria Koutsoukalis <mkoutsoukalis@outlook.com>
2023-08-22 15:15:42 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+let employee = {
+    name: "Maria",
+    streetAddress: "11 Broadway"
+  };
+  // 1. updateEmployeeWithKeyAndValue()
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  // 2. destructivelyUpdateEmployeeWithKeyAndValue()
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // 3. deleteFromEmployeeByKey()
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // 4. destructivelyDeleteFromEmployeeByKey()
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
